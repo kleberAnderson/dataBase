355683da852272bd21a1b6347866b0fa121b487f
Anderson Kleber <a.velasco.kleber@gmail.com>
2026-03-09 00:19:36 -0300

    adding news exercises

--- exercicio-local/programacao/03programacao.sql
+++ exercicio-local/programacao/03programacao.sql
@@ -185,6 +185,29 @@ END
 SELECT * FROM livro
 
 --04 PROGRAMAÇÃO SQL
+
+--a) Fazer um algoritmo que leia 1 número e mostre se são múltiplos de 2,3,5 ou nenhum deles
+DECLARE @numero INT
+
+SET @numero = 1
+
+IF (@numero % 2 = 0)
+BEGIN
+	PRINT('multiplo de 2')
+END
+IF (@numero % 3 = 0)
+BEGIN
+	PRINT('multiplo de 3')
+END
+IF (@numero % 5 = 0)
+BEGIN
+	PRINT('multiplo de 5')
+END
+IF (@numero % 2 <> 0 AND @numero % 3 <> 0 AND @numero % 5 <> 0)
+BEGIN
+	PRINT('O número ' + CAST((@numero) AS VARCHAR) + ' nao é multiplo de 2, 3 e 5.')
+END
+
 --b) Fazer um algoritmo que leia 3 números e mostre o maior e o menor
 DECLARE @numero1 INT,
 		@numero2 INT,
@@ -208,3 +231,146 @@ IF( @numero2 < @menor_num ) BEGIN SET @menor_num = @numero2 END
 IF( @numero3 < @menor_num ) BEGIN SET @menor_num = @numero3 END
 
 SELECT @maior_num AS maior, @menor_num AS menor
+
+--c) Fazer um algoritmo que calcule os 15 primeiros termos da série
+--1,1,2,3,5,8,13,21,...
+--E calcule a soma dos 15 termos
+DECLARE @a INT,
+		@b INT,
+		@c INT,
+		@cont INT,
+		@soma INT
+
+SET @a = 1
+SET @b = 1
+SET @cont = 2
+SET @soma = @a + @b
+
+PRINT(@a)
+PRINT(@b)
+
+WHILE (@cont < 15)
+BEGIN
+	SET @c = @a + @b
+	PRINT (@c)
+
+	SET @soma = @soma + @c
+
+	SET @a = @b
+	SET @b = @c
+
+	SET @cont = @cont + 1
+END
+
+PRINT ('Soma dos 15 primeiros termos -> ' + CAST(@soma AS VARCHAR))
+
+--d) Fazer um algoritmo que separa uma frase, colocando todas as letras em maiúsculo e em
+--minúsculo (Usar funções UPPER e LOWER)
+DECLARE @frase VARCHAR(30)
+
+SET @frase = ('FaTeC zOnA LeSTe')
+
+PRINT ('Maiusculo -> ' + UPPER(@frase))
+PRINT ('Minusculo -> ' + LOWER(@frase))
+
+--e) Fazer um algoritmo que inverta uma palavra (Usar a função SUBSTRING)
+DECLARE @palavra VARCHAR(30)
+DECLARE @palavra_invertida VARCHAR(30)
+DECLARE @num_palavra INT
+
+SET @palavra = 'FATEC ZONA LESTE'
+SET @palavra_invertida = ''
+SET @num_palavra = LEN(@palavra)
+
+WHILE (@num_palavra > 0)
+BEGIN
+	SET @palavra_invertida = @palavra_invertida + SUBSTRING(@palavra, @num_palavra, 1)
+	SET @num_palavra = @num_palavra - 1
+END
+
+PRINT('Palavra invertida -> ' + @palavra_invertida)
+/*
+f) Considerando a tabela abaixo, gere uma massa de dados, com 100 registros, para fins de teste
+com as regras estabelecidas (Não usar constraints na criação da tabela)
+Computador
+ID Marca QtdRAM TipoHD QtdHD FreqCPU
+INT (PK) VARCHAR(40) INT VARCHAR(10) INT DECIMAL(7,2)
+
+• ID incremental a iniciar de 10001
+• Marca segue o padrão simples, Marca 1, Marca 2, Marca 3, etc.
+• QtdRAM é um número aleatório* dentre os valores permitidos (2, 4, 8, 16)
+• TipoHD segue o padrão:
+o Se o ID dividido por 3 der resto 0, é HDD
+o Se o ID dividido por 3 der resto 1, é SSD
+o Se o ID dividido por 3 der resto 2, é M2 NVME
+• QtdHD segue o padrão:
+o Se o TipoHD for HDD, um valor aleatório* dentre os valores permitidos (500, 1000 ou 2000)
+o Se o TipoHD for SSD, um valor aleatório* dentre os valores permitidos (128, 256, 512)
+• FreqHD é um número aleatório* entre 1.70 e 3.20
+
+* Função RAND() gera números aleatórios entre 0 e 0,9999...
+*/
+CREATE TABLE computador (
+id INT,
+marca VARCHAR(30),
+qtd_ram INT,
+tipo_hd VARCHAR(30),
+qtd_hd INT,
+frq_cpu DECIMAL(7,2)
+)
+
+DECLARE @id INT,
+		@contador INT,
+		@marca VARCHAR(30),
+		@qtdram INT,
+		@tipohd VARCHAR(30),
+		@qtdhd INT,
+		@freqcpu DECIMAL(7,2)
+
+SET @id = 10001
+SET @contador = 1
+
+WHILE (@contador <= 100)
+BEGIN
+	SET @marca = 'Marca ' + CAST(@contador AS VARCHAR)
+	SET @qtdram = CASE FLOOR(RAND()  * 4)
+					WHEN 0 THEN 2
+					WHEN 1 THEN 4
+					WHEN 2 THEN 8
+					WHEN 3 THEN 16
+					ELSE 16
+					END
+	SET @tipohd = CASE (@id % 3)
+					WHEN 0 THEN 'HDD'
+					WHEN 1 THEN 'SSD'
+					WHEN 2 THEN 'M2 NVME'
+					END
+	IF (@tipohd = 'HDD')
+	BEGIN
+		SET @qtdhd = CASE FLOOR(RAND() * 3)
+						WHEN 0 THEN 500
+						WHEN 1 THEN 1000
+						WHEN 2 THEN 2000
+						ELSE 500
+						END
+	END
+	ELSE
+	BEGIN
+		SET @qtdhd = CASE FLOOR(RAND() * 3)
+						WHEN 0 THEN 128
+						WHEN 1 THEN 256
+						WHEN 2 THEN 512
+						ELSE 128
+						END
+	END
+
+	SET @freqcpu = 1.70 + (RAND() * (3.20 - 1.70))
+
+	INSERT INTO computador VALUES(
+	@id, @marca, @qtdram, @tipohd, @qtdhd, @freqcpu)
+
+	SET @id = @id + 1
+	SET @contador = @contador + 1
+END
+
+SELECT * FROM computador
